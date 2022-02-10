0654c369e5cf909a6374c01e9f6dfa4a813c88e0
Azamat542 <75069360+Azamat542@users.noreply.github.com>
2022-02-10 17:44:58 +0600

    Delete SQLQuery19.sql

--- SQLQuery19.sql
+++
@@ -1,9 +0,0 @@
-/****** Скрипт для команды SelectTopNRows из среды SSMS  ******/
-SELECT TOP (1000) [Id]
-      ,[FirstName]
-      ,[LastName]
-      ,[Patronymic]
-      ,[Iin]
-      ,[DateOfBirht]
-      ,[GenderId]
-  FROM [Shop2].[dbo].[Clients]
